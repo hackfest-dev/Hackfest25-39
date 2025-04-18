4e276ddeb747db2175fb31f085e8bd582e580801
Banudeep <banudeep8277@gmail.com>
2025-04-18 18:55:32 +0530

    homepage imported

--- carbonemission/src/App.jsx
+++ carbonemission/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-
+import Homepage from './components/Homepage/Homepage'
 import './App.css';
 
 function App() {
