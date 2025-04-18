bb4528c28d090f1a5f374a09f234a3847cf64287
Banudeep <banudeep8277@gmail.com>
2025-04-18 22:49:48 +0530

    updated app

--- carbonemission/src/App.jsx
+++ carbonemission/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage';
+import Aboutus from './components/Aboutus/Aboutus';
 import Login_opt from './components/Login/Login_opt/Login_opt';
 import Adminlogin from './components/Login/Admin/Adminlogin';
 import AdministratorLogin from './components/Login/Administrator/AdministratorLogin';
@@ -15,8 +16,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        
-        <Route path="/" element={<Homepage />} />
+        <Route path="/aboutus" element={<Aboutus />} />        
         <Route path="/adminlogin" element={<Adminlogin />} />
         <Route path="/login_opt" element={<Login_opt />} />
         <Route path="/administratorlogin" element={<AdministratorLogin />} />
