ab0652cd97703a1af1cdf968c62d44d56db5b4e3
Banudeep <banudeep8277@gmail.com>
2025-04-18 22:40:11 +0530

    admin app

--- carbonemission/src/App.jsx
+++ carbonemission/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage';
 import Login_opt from './components/Login/Login_opt/Login_opt';
-import Adminlogin from './components/Login/Adminlogin/Adminlogin';
+import Adminlogin from './components/Login/Admin/Adminlogin';
 import AdministratorLogin from './components/Login/AdministratorLogin/AdministratorLogin';
 import AdministratorSignup from './components/Login/AdministratorSignup/AdministratorSignup';
 import Sectorlogin from './components/Login/Sectorlogin/Sectorlogin';
