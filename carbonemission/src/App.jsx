5df7686c2f84b478ddba04e2fc34b9c8d0732a28
Im00000zero <sambram.v76@gmail.com>
2025-04-18 22:18:45 +0530

    adding home page to app

--- carbonemission/src/App.jsx
+++ carbonemission/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Homepage from './components/Homepage/Homepage'
+import Homepage from './components/Homepage/Homepage';
+import Login_opt from './components/Login/Login_opt/Login_opt'
 import './App.css';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login_opt" element={<Login_opt />} />
         
 
       </Routes>
